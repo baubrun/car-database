246910702532689b2f103dd884199471bab81d63
baubrun <baubelf@gmail.com>
2021-09-16 12:23:34 -0400

    added cors

---
+++ src/main/java/com/bb/cardatabase/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.bb.cardatabase;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource(){
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(List.of("*"));
+        config.setAllowedMethods(List.of("*"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowCredentials(true);
+
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
+}
--- src/test/java/com/bb/cardatabase/CardatabaseApplicationTests.java
+++ src/test/java/com/bb/cardatabase/CardatabaseApplicationTests.java
@@ -1,13 +1,23 @@
 package com.bb.cardatabase;
 
+import com.bb.cardatabase.car.CarController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
 
 @SpringBootTest
 class CardatabaseApplicationTests {
 
+	@Autowired
+	private CarController controller;
+
+
 	@Test
 	void contextLoads() {
+		assert controller != null;
 	}
+
+
 
 }
